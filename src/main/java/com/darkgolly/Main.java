50a893d0d2e361f7db5f583e1eec9ab2e823a8af
Danya <67584569+DarkGolly@users.noreply.github.com>
2026-04-07 13:48:30 +0300

    fix: Added support for end-to-end encryption of DAVE, update versions dependencies

--- src/main/java/com/darkgolly/Main.java
+++ src/main/java/com/darkgolly/Main.java
@@ -1,11 +1,14 @@
 package com.darkgolly;
 
 import com.darkgolly.listeners.CommandListener;
+import club.minnced.discord.jdave.interop.JDaveSessionFactory;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.audio.AudioModuleConfig;
 import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.slf4j.Logger;
@@ -16,9 +19,14 @@ import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 public class Main {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    static void main() {
+        AudioModuleConfig audioConfig = new AudioModuleConfig()
+                .withDaveSessionFactory(new JDaveSessionFactory());
+
         JDA jda = JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_VOICE_STATES)
+                .enableCache(CacheFlag.VOICE_STATE)
+                .setAudioModuleConfig(audioConfig)
                 .addEventListeners(new CommandListener())
                 .build();
         log.info("Bot started");
--- src/main/java/com/darkgolly/commands/JoinCommand.java
+++ src/main/java/com/darkgolly/commands/JoinCommand.java
@@ -1,20 +1,30 @@
 package com.darkgolly.commands;
 
+import com.darkgolly.audio.GuildMusicManager;
+import com.darkgolly.audio.PlayerManager;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.Objects;
 
 public class JoinCommand {
 
     public void execute(SlashCommandInteractionEvent event) {
-        AudioChannel channel = event.getMember().getVoiceState().getChannel();
+        AudioChannel channel = Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getVoiceState()).getChannel();
 
         if (channel == null) {
             event.getChannel().sendMessage("Вы не находитесь в голосовом канале!").queue();
             return;
         }
 
-        event.getGuild().getAudioManager().openAudioConnection(channel);
+        Guild guild = Objects.requireNonNull(event.getGuild());
+        GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(guild);
+        if (guild.getAudioManager().getSendingHandler() == null) {
+            guild.getAudioManager().setSendingHandler(musicManager.getSendHandler());
+        }
+
+        guild.getAudioManager().openAudioConnection(channel);
         event.reply("Подключился к голосовому каналу!").queue();
     }
 }
